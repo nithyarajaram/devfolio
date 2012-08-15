450a653a5d58628e70b176292568ff571dce3dd4
Nithya Rajaram <nithya@muziboo.com>
2012-08-15 18:05:03 -0400

    Devfolio with devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Devfolio::Application.config.secret_token = 'e8d5bff4d82cf32e07ac48cd040bfc2e7acd3a44d04ef7338f003697b5bdfd0d735cdb06b704fc8b0f182d02c14ab89412ba659b8a786a1a79d0d96080498102'
