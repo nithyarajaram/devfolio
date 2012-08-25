a1bac483228a3e8a9db0628641d046c890a0b6cd
Nithya Rajaram <nithya@muziboo.com>
2012-08-25 01:10:57 -0400

    adding Gravtar to users

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem "pg", :group => :production
 gem "sqlite3", :group => :development
 
 gem 'carmen-rails'
+gem 'gravtastic'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+  include Gravtastic
+  gravtastic 
   
   has_one :portfolio, dependent: :destroy, :foreign_key => "user_id"
 end
