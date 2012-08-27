4a0bf2d3ee629fe25dc036d4de8263bd0c7ec522
Nithya Rajaram <nithya@muziboo.com>
2012-08-27 19:48:52 -0400

    Added dropdown to budget and location on mainpage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,17 @@ class HomeController < ApplicationController
  
   def index
     @portfolios = Portfolio.includes(:user).all
+    @portfolios_location = location_list
+    @portfolios_budget = budget_list
   end
+
+  def location_list
+    Portfolio.select("distinct(location)").where("location IS NOT NULL").each {|portfolio| portfolio.location} 
+  end    
+  
+  def budget_list
+     Portfolio.select("distinct(budget)").where("budget IS NOT NULL").each {|portfolio| portfolio.budget} 
+  end   
  
 end
 
